50428f5501b2cf0e69474c4a3a859ae27af34023
Miquel Brazil <mbrazil@thingwone.com>
2014-03-30 22:40:17 -0400

    incremented version number for code transfer from ArtCares2014 template

--- artshares-data-manager.php
+++ artshares-data-manager.php
@@ -4,7 +4,7 @@
 Plugin Name:    ArtShares Data Manager
 Plugin URI:     http://aidstaskforce.org/technology
 Description:    Wordpress plugin that interfaces with the ATGC Formstack account and the forms specific to the ArtCares event process.
-Version:        0.0.1
+Version:        0.0.5
 Author:         Miquel Brazil [thingwone]
 Author URI:     http://thingworld.com/aboutme
 License:        MIT
