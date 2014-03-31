7cb65496ff07d11223ab6d6eddcad13cb9eaecfd
Miquel Brazil <mbrazil@thingwone.com>
2014-03-30 22:43:40 -0400

    defined variables

--- artshares-data-manager.php
+++ artshares-data-manager.php
@@ -10,4 +10,14 @@ Author URI:     http://thingworld.com/aboutme
 License:        MIT
 */
 
+define('CLIENT_ID', '11880');
+define('CLIENT_SECRET', '98e576a1c6');
+define('REDIRECT_URL', get_permalink());
+
+define('SERVICE_URL', 'https://www.formstack.com/api/v2');
+define('AUTHORIZE_EP', '/oauth2/authorize');
+define('AUTHORIZE_URL', SERVICE_URL.AUTHORIZE_EP);
+define('TOKEN_EP', '/oauth2/token');
+define('TOKEN_URL', SERVICE_URL.TOKEN_EP);
+
 ?>
