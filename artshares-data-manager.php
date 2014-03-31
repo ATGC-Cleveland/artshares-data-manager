efa8e72c985e2b100804dac0a02ca918244aacc0
Miquel Brazil <mbrazil@thingwone.com>
2014-03-30 22:34:28 -0400

    added plugin setup file

---
+++ artshares-data-manager.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name:    ArtShares Data Manager
+Plugin URI:     http://aidstaskforce.org/technology
+Description:    Wordpress plugin that interfaces with the ATGC Formstack account and the forms specific to the ArtCares event process.
+Version:        0.0.1
+Author:         Miquel Brazil [thingwone]
+Author URI:     http://thingworld.com/aboutme
+License:        MIT
+*/
+
+?>
